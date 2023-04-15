3536f04bfa561ebcdb99117e3768d40262ab457a
junbum766 <junbum766@gmail.com>
2023-04-15 15:59:05 +0900

    230415 new

--- chat.js
+++ chat.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 const corsOptions = {
   origin: "https://stdict.korean.go.kr/api/search.do",
 };
